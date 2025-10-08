b1bef5ede61c374f8ae450d51b3181b71eb957fa
sabilaindah5-stack <sabilaindah5@gmail.com>
2025-10-08 11:09:01 +0700

    Add files via upload

---
+++ list_buah = ['Mangga', 'Jeruk', 'zaitun' 8 oktober.py
@@ -0,0 +1,10 @@
+list_buah = ['Mangga', 'Jeruk', 'zaitun', 'Apel', 'Durian']
+print(list_buah)
+
+# urutkan secara ascending
+list_buah.sort()
+print(list_buah)
+
+# membalikan posisi item list (tidak harus berururut)
+list_buah.reverse()
+print(list_buah)
