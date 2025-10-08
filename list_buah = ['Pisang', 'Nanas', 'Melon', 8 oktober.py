c3c8e8f5c22f0fc81e4ec453903becba3b760cb8
sabilaindah5-stack <sabilaindah5@gmail.com>
2025-10-08 11:08:37 +0700

    Add files via upload

---
+++ list_buah = ['Pisang', 'Nanas', 'Melon', 8 oktober.py
@@ -0,0 +1,15 @@
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian',]
+print(list_buah)
+
+# hapus item pertama dengan nilai 'jambu'
+list_buah.remove('jambu')
+print(list_buah)
+
+list_buah =  ['Mangga', 'jambu', 'jeruk', 'jambu']
+print(list_buah)
+
+del list_buah[1]
+print(list_buah)
+
+del list_buah[0:2]
+print(list_buah)
